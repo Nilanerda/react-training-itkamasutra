211e38f83a2a2462f57f7ac01bf8fa115363a4e7
Yana <zzizzu@ukr.net>
2020-02-27 14:53:21 +0200

    created basic layout for next training with react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header className="app-header">
+                <div className="inner-container">
+                    <a href="/"></a>
+                </div>
+            </header>
+            <nav className="app-nav">
+                <div className="inner-container">
+                    <ul className="app-nav-panel">
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="#">Messages</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Music</a></li>
+                        <li><a href="#">Settings</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <main className="app-main">
+                <div className="inner-container">
+                    <div className="main-wrapper-picture"></div>
+                    <div className="main-info-wrapper">
+                        <div className="main-profile-info">
+                            <div className="profile-avatar"></div>
+                            <div className="profile-info">
+                                <ul className="profile-info-data">
+                                    <li>Name</li>
+                                    <li>Date of Birth:</li>
+                                    <li>City:</li>
+                                    <li>Education:</li>
+                                    <li>Web site:</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="main-posts-field">
+                            <p>My posts</p>
+                            <textarea name="new-post-field"
+                                      id="newPostField"
+                                      cols="1"
+                                      rows="4"
+                                      placeholder="your news..."></textarea>
+                            <button>Send</button>
+                            <article className="post-instance">
+                                <div className="post-instance-wrapper">
+                                    <div className="post-instance-avatar"></div>
+                                    <div className="post-data">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                           Assumenda atque dicta dolore dolorum ex id in necessitatibus
+                                           numquam pariatur perspiciatis praesentium quae quam rem
+                                           soluta tempora temporibus vel vero, voluptatum. Lorem ipsum dolor sit amet,
+                                           consectetur adipisicing elit. Accusantium ad adipisci aliquam assumenda
+                                           consectetur dolorum ipsam omnis, ratione saepe veniam! Assumenda corporis
+                                           inventore iusto labore magni quam quisquam repellat sint! Lorem ipsum dolor
+                                           sit amet, consectetur adipisicing elit. Accusamus amet at explicabo harum
+                                           iste nemo quam quisquam sequi sunt vero! Blanditiis dolorem, est
+                                           exercitationem magnam minus molestiae nemo temporibus voluptatem?</p>
+                                    </div>
+                                </div>
+                            </article>
+                            <article className="post-instance">
+                                <div className="post-instance-wrapper">
+                                    <div className="post-instance-avatar"></div>
+                                    <div className="post-data">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                           Assumenda atque dicta dolore dolorum ex id in necessitatibus
+                                           numquam pariatur perspiciatis praesentium quae quam rem
+                                           soluta tempora temporibus vel vero, voluptatum. Lorem ipsum dolor sit amet,
+                                           consectetur adipisicing elit. Accusantium ad adipisci aliquam assumenda
+                                           consectetur dolorum ipsam omnis, ratione saepe veniam! Assumenda corporis
+                                           inventore iusto labore magni quam quisquam repellat sint! Lorem ipsum dolor
+                                           sit amet, consectetur adipisicing elit. Accusamus amet at explicabo harum
+                                           iste nemo quam quisquam sequi sunt vero! Blanditiis dolorem, est
+                                           exercitationem magnam minus molestiae nemo temporibus voluptatem?</p>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
 }
 
-export default App;
+export default App
