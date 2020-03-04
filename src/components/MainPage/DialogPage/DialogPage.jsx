6e5f2df7a3865172b9963fbac1e2eebd9eecdb0e
Yana <zzizzu@ukr.net>
2020-03-04 12:14:29 +0200

    created recipient list field, fixed header style

--- src/components/MainPage/DialogPage/DialogPage.jsx
+++ src/components/MainPage/DialogPage/DialogPage.jsx
@@ -3,7 +3,52 @@ import styles from './DialogPage.module.scss';
 
 const DialogPage = () => {
     return (
-        <div className={styles.innerContainer}>asd</div>
+        <div className={styles.innerContainer}>
+            <div className={styles.recipientListField}>
+                <div className={styles.recipientSearchField}>
+                    <input type="search" placeholder='Search'/>
+                </div>
+                <div className={styles.recipientList}>
+                    <div className={styles.recipientInstance}>
+                        <div className={styles.recipientAvatar}></div>
+                        <div className={styles.recipientName}>Vasya</div>
+                    </div>
+                    <div className={styles.recipientInstance}>
+                        <div className={styles.recipientAvatar}></div>
+                        <div className={styles.recipientName}>Vasya</div>
+                    </div>
+                    <div className={styles.recipientInstance}>
+                        <div className={styles.recipientAvatar}></div>
+                        <div className={styles.recipientName}>Vasya</div>
+                    </div>
+                    <div className={styles.recipientInstance}>
+                        <div className={styles.recipientAvatar}></div>
+                        <div className={styles.recipientName}>Vasya</div>
+                    </div>
+                    <div className={styles.recipientInstance}>
+                        <div className={styles.recipientAvatar}></div>
+                        <div className={styles.recipientName}>Vasya</div>
+                    </div>
+                    <div className={styles.recipientInstance}>
+                        <div className={styles.recipientAvatar}></div>
+                        <div className={styles.recipientName}>Vasya</div>
+                    </div>
+                    <div className={styles.recipientInstance}>
+                        <div className={styles.recipientAvatar}></div>
+                        <div className={styles.recipientName}>Vasya</div>
+                    </div>
+                    <div className={styles.recipientInstance}>
+                        <div className={styles.recipientAvatar}></div>
+                        <div className={styles.recipientName}>Vasya</div>
+                    </div>
+                    <div className={styles.recipientInstance}>
+                        <div className={styles.recipientAvatar}></div>
+                        <div className={styles.recipientName}>Vasya</div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.chatField}>a</div>
+        </div>
     )
 };
 
