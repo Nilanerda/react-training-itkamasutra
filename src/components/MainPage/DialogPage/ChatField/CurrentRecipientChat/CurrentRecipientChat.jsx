66aef2eb3de5cf863bc2d806390ed6f49dc2bcb8
Yana <zzizzu@ukr.net>
2020-03-16 17:17:58 +0200

    transformed hardcode tags into data coming from array

--- src/components/MainPage/DialogPage/ChatField/CurrentRecipientChat/CurrentRecipientChat.jsx
+++ src/components/MainPage/DialogPage/ChatField/CurrentRecipientChat/CurrentRecipientChat.jsx
@@ -3,10 +3,25 @@ import styles from './CurrentRecipientChat.module.scss';
 import MessageInstance from "./MessageInstance/MessageInstance";
 
 const CurrentRecipientChat = () => {
+
+    let messagesData = [
+        {id: 1, message: 'Hi guys'},
+        {id: 2, message: 'What\'s up?'},
+        {id: 3, message: 'Svetlana is so pretty'},
+        {id: 4, message: 'Dmytro hello'},
+        {id: 5, message: 'Guy where is my money hah?'},
+        {id: 6, message: 'Love peace'}
+    ];
+
+    let messagesRenderData = messagesData.map( (messageData) => {
+        return (
+            <MessageInstance messageStyleType='sentMessage' message={messageData.message}/>
+        )
+    });
+
     return (
         <div className={styles.currentRecipientChat}>
-            <MessageInstance messageStyleType='sentMessage' message='Blah'/>
-            <MessageInstance messageStyleType='receivedMessage' message='Lorem'/>
+            {messagesRenderData}
         </div>
     )
 };
--- src/components/MainPage/DialogPage/RecipientListField/RecipientList/RecipientList.jsx
+++ src/components/MainPage/DialogPage/RecipientListField/RecipientList/RecipientList.jsx
@@ -3,9 +3,22 @@ import styles from './RecipientList.module.scss';
 import RecipientInstance from "./RecipientInstance/RecipientInstance";
 
 const RecipientList = () => {
+    let recipientData = [
+        {id: 1, name: 'Vasya'},
+        {id: 2, name: 'Petro'},
+        {id: 3, name: 'Svetlana'},
+        {id: 4, name: 'Dmytro'},
+        {id: 5, name: 'Pavel'},
+        {id: 6, name: 'Anna'}
+    ];
+
+    let recipientRenderData = recipientData.map((recipient) => {
+        return (<RecipientInstance name={recipient.name} id={recipient.id}/>)
+    });
+
     return (
         <div className={styles.recipientList}>
-            <RecipientInstance/>
+            {recipientRenderData}
         </div>
     )
 };
