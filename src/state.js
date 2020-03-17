3f429c98245ae9a997d270a8d4ac771fe91bffb7
Yana <zzizzu@ukr.net>
2020-03-17 19:04:23 +0200

    created state.js for all data imitation, moved data into this file

---
+++ src/state.js
@@ -0,0 +1,22 @@
+let state = {
+    dialogPage: {
+        messagesData: [
+            {id: 1, message: 'Hi guys'},
+            {id: 2, message: 'What\'s up?'},
+            {id: 3, message: 'Svetlana is so pretty'},
+            {id: 4, message: 'Dmytro hello'},
+            {id: 5, message: 'Guy where is my money hah?'},
+            {id: 6, message: 'Love peace'}
+        ],
+        recipientData: [
+            {id: 1, name: 'Vasya'},
+            {id: 2, name: 'Petro'},
+            {id: 3, name: 'Svetlana'},
+            {id: 4, name: 'Dmytro'},
+            {id: 5, name: 'Pavel'},
+            {id: 6, name: 'Anna'}
+        ]
+    }
+};
+
+export default state;
