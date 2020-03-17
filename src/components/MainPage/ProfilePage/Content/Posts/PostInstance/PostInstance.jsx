9442f32c9c20dd0458fa29c5e22e4e9fde34d15b
Yana <zzizzu@ukr.net>
2020-03-17 20:58:41 +0200

    divided PostInstance component into subcomponent

--- src/components/MainPage/ProfilePage/Content/Posts/PostInstance/PostInstance.jsx
+++ src/components/MainPage/ProfilePage/Content/Posts/PostInstance/PostInstance.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 import './PostInstance.scss';
+import PostInstanceData from "./PostInstanceData/PostInstanceData";
 
 const PostInstance = () => {
     return (
         <article className="post-instance">
             <div className="post-instance-wrapper">
                 <div className="post-instance-avatar"></div>
-                <div className="post-data">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                       Assumenda atque dicta dolore dolorum ex id in necessitatibus
-                       numquam pariatur perspiciatis praesentium quae quam rem
-                       soluta tempora temporibus vel vero, voluptatum. Lorem ipsum dolor sit amet,
-                       consectetur adipisicing elit. Accusantium ad adipisci aliquam assumenda
-                       consectetur dolorum ipsam omnis, ratione saepe veniam! Assumenda corporis
-                       inventore iusto labore magni quam quisquam repellat sint! Lorem ipsum dolor
-                       sit amet, consectetur adipisicing elit. Accusamus amet at explicabo harum
-                       iste nemo quam quisquam sequi sunt vero! Blanditiis dolorem, est
-                       exercitationem magnam minus molestiae nemo temporibus voluptatem?</p>
-                </div>
+                <PostInstanceData/>
             </div>
         </article>
     )
---
+++ src/components/MainPage/ProfilePage/Content/Posts/PostInstance/PostInstanceData/PostInstanceData.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './PostInstanceData.scss';
+
+const PostInstanceData = () => {
+    return (
+        <div className="post-data">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+               Assumenda atque dicta dolore dolorum ex id in necessitatibus
+               numquam pariatur perspiciatis praesentium quae quam rem
+               soluta tempora temporibus vel vero, voluptatum. Lorem ipsum dolor sit amet,
+               consectetur adipisicing elit. Accusantium ad adipisci aliquam assumenda
+               consectetur dolorum ipsam omnis, ratione saepe veniam! Assumenda corporis
+               inventore iusto labore magni quam quisquam repellat sint! Lorem ipsum dolor
+               sit amet, consectetur adipisicing elit. Accusamus amet at explicabo harum
+               iste nemo quam quisquam sequi sunt vero! Blanditiis dolorem, est
+               exercitationem magnam minus molestiae nemo temporibus voluptatem?</p>
+        </div>
+    )
+};
+
+export default PostInstanceData;
