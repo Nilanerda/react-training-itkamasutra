095bd34b8e92f6fe557762d5e7f18c510af0931f
Yana <zzizzu@ukr.net>
2020-03-18 20:48:04 +0200

    fixed styles for DialogPage components

--- src/components/MainPage/DialogPage/ChatField/ChatField.jsx
+++ src/components/MainPage/DialogPage/ChatField/ChatField.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./ChatField.module.scss";
 import CurrentRecipientChat from "./CurrentRecipientChat/CurrentRecipientChat";
+import MessageInput from "./MessageInput/MessageInput";
 
 const ChatField = (props) => {
     return (
         <div className={styles.chatField}>
             <CurrentRecipientChat currentDialogState={props.dialogPageState.messagesData}/>
+            <MessageInput />
         </div>
     )
 };
