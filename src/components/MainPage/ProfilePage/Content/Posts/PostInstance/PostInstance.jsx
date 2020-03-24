1f7a579d05c047f6ab72373062909ef326419046
Yana <zzizzu@ukr.net>
2020-03-24 12:51:03 +0200

    fixed post instance avatar styles

--- src/components/MainPage/ProfilePage/Content/Posts/PostInstance/PostInstance.jsx
+++ src/components/MainPage/ProfilePage/Content/Posts/PostInstance/PostInstance.jsx
@@ -6,7 +6,9 @@ const PostInstance = (props) => {
     return (
         <article className="post-instance">
             <div className="post-instance-wrapper">
-                <div className="post-instance-avatar"></div>
+                <div className="avatar-field">
+                    <div className="post-instance-avatar"></div>
+                </div>
                 <PostInstanceData post={props.post}/>
             </div>
         </article>
