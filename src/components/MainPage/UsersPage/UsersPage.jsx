a2f1bef3d8e42eaab68bd0b4d0fa82b9b4710019
Yana <zzizzu@ukr.net>
2020-04-18 22:16:56 +0300

    created markup for UsersPage component

--- src/components/MainPage/UsersPage/UsersPage.jsx
+++ src/components/MainPage/UsersPage/UsersPage.jsx
@@ -1,8 +1,133 @@
 import React from "react";
+import styles from './UsersPage.module.scss';
 
 let UsersPage = () => {
     return (
-        <div>users</div>
+        <div className={styles.innerContainer}>
+            <div className={styles.usersAreaContainer}>
+                <div className={styles.userInstance}>
+                    <div className={styles.userInstanceAvatar}></div>
+                    <div className={styles.userInstanceInfo}>
+                        <ul>
+                            <li className={styles.userInstanceInfoName}>
+                                <a href="">Name</a>
+                            </li>
+                            <li>City, Country</li>
+                            <li className={styles.userInstanceInfoStatus}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda at consectetur debitis dignissimos dolorum eos eveniet, ex, illo laborum magni modi nam nisi nobis perspiciatis qui repellendus sapiente sunt.</li>
+                        </ul>
+                    </div>
+                    <div className={styles.userInstanceFollowButton}>
+                        <button>Follow</button>
+                    </div>
+                </div>
+                <div className={styles.userInstance}>
+                    <div className={styles.userInstanceAvatar}></div>
+                    <div className={styles.userInstanceInfo}>
+                        <ul>
+                            <li className={styles.userInstanceInfoName}>
+                                <a href="">Name</a>
+                            </li>
+                            <li>City, Country</li>
+                            <li className={styles.userInstanceInfoStatus}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda at consectetur debitis dignissimos dolorum eos eveniet, ex, illo laborum magni modi nam nisi nobis perspiciatis qui repellendus sapiente sunt.</li>
+                        </ul>
+                    </div>
+                    <div className={styles.userInstanceFollowButton}>
+                        <button>Follow</button>
+                    </div>
+                </div>
+                <div className={styles.userInstance}>
+                    <div className={styles.userInstanceAvatar}></div>
+                    <div className={styles.userInstanceInfo}>
+                        <ul>
+                            <li className={styles.userInstanceInfoName}>
+                                <a href="">Name</a>
+                            </li>
+                            <li>City, Country</li>
+                            <li className={styles.userInstanceInfoStatus}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda at consectetur debitis dignissimos dolorum eos eveniet, ex, illo laborum magni modi nam nisi nobis perspiciatis qui repellendus sapiente sunt.</li>
+                        </ul>
+                    </div>
+                    <div className={styles.userInstanceFollowButton}>
+                        <button>Follow</button>
+                    </div>
+                </div>
+                <div className={styles.userInstance}>
+                    <div className={styles.userInstanceAvatar}></div>
+                    <div className={styles.userInstanceInfo}>
+                        <ul>
+                            <li className={styles.userInstanceInfoName}>
+                                <a href="">Name</a>
+                            </li>
+                            <li>City, Country</li>
+                            <li className={styles.userInstanceInfoStatus}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda at consectetur debitis dignissimos dolorum eos eveniet, ex, illo laborum magni modi nam nisi nobis perspiciatis qui repellendus sapiente sunt.</li>
+                        </ul>
+                    </div>
+                    <div className={styles.userInstanceFollowButton}>
+                        <button>Follow</button>
+                    </div>
+                </div>
+                <div className={styles.userInstance}>
+                    <div className={styles.userInstanceAvatar}></div>
+                    <div className={styles.userInstanceInfo}>
+                        <ul>
+                            <li className={styles.userInstanceInfoName}>
+                                <a href="">Name</a>
+                            </li>
+                            <li>City, Country</li>
+                            <li className={styles.userInstanceInfoStatus}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda at consectetur debitis dignissimos dolorum eos eveniet, ex, illo laborum magni modi nam nisi nobis perspiciatis qui repellendus sapiente sunt.</li>
+                        </ul>
+                    </div>
+                    <div className={styles.userInstanceFollowButton}>
+                        <button>Follow</button>
+                    </div>
+                </div>
+                <div className={styles.userInstance}>
+                    <div className={styles.userInstanceAvatar}></div>
+                    <div className={styles.userInstanceInfo}>
+                        <ul>
+                            <li className={styles.userInstanceInfoName}>
+                                <a href="">Name</a>
+                            </li>
+                            <li>City, Country</li>
+                            <li className={styles.userInstanceInfoStatus}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda at consectetur debitis dignissimos dolorum eos eveniet, ex, illo laborum magni modi nam nisi nobis perspiciatis qui repellendus sapiente sunt.</li>
+                        </ul>
+                    </div>
+                    <div className={styles.userInstanceFollowButton}>
+                        <button>Follow</button>
+                    </div>
+                </div>
+                <div className={styles.userInstance}>
+                    <div className={styles.userInstanceAvatar}></div>
+                    <div className={styles.userInstanceInfo}>
+                        <ul>
+                            <li className={styles.userInstanceInfoName}>
+                                <a href="">Name</a>
+                            </li>
+                            <li>City, Country</li>
+                            <li className={styles.userInstanceInfoStatus}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda at consectetur debitis dignissimos dolorum eos eveniet, ex, illo laborum magni modi nam nisi nobis perspiciatis qui repellendus sapiente sunt.</li>
+                        </ul>
+                    </div>
+                    <div className={styles.userInstanceFollowButton}>
+                        <button>Follow</button>
+                    </div>
+                </div>
+                <div className={styles.userInstance}>
+                    <div className={styles.userInstanceAvatar}></div>
+                    <div className={styles.userInstanceInfo}>
+                        <ul>
+                            <li className={styles.userInstanceInfoName}>
+                                <a href="">Name</a>
+                            </li>
+                            <li>City, Country</li>
+                            <li className={styles.userInstanceInfoStatus}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam assumenda at consectetur debitis dignissimos dolorum eos eveniet, ex, illo laborum magni modi nam nisi nobis perspiciatis qui repellendus sapiente sunt.</li>
+                        </ul>
+                    </div>
+                    <div className={styles.userInstanceFollowButton}>
+                        <button>Follow</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
