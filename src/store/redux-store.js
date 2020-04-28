ec0a245d2c47f0481c92edd97ecdeabeb5acbab1
Yana <zzizzu@ukr.net>
2020-04-28 11:45:44 +0300

    add usersPage reducer into combine

--- src/store/redux-store.js
+++ src/store/redux-store.js
@@ -1,10 +1,12 @@
 import {combineReducers, createStore} from "redux";
 import dialogPageReducer from "./dialog-page-reducer";
 import profilePageReducer from "./profile-page-reducer";
+import usersPageReducer from "./users-page-reducer";
 
 let reducersCollection = combineReducers({
     profilePage: profilePageReducer,
-    dialogPage: dialogPageReducer
+    dialogPage: dialogPageReducer,
+    usersPage: usersPageReducer
 });
 
 let store = createStore(reducersCollection);
