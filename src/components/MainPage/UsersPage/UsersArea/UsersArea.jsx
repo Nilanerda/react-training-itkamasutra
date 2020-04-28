49d6782f2ea9c46c5bd07c3249d64b322ac96335
Yana <zzizzu@ukr.net>
2020-04-28 14:41:59 +0300

    created imitation of usersData loading from backend

--- src/components/MainPage/UsersPage/UsersArea/UsersArea.jsx
+++ src/components/MainPage/UsersPage/UsersArea/UsersArea.jsx
@@ -2,6 +2,56 @@ import React from "react";
 import styles from './UsersArea.module.scss';
 
 const UsersArea = (props) => {
+
+    if (props.usersData.length === 0) {
+        props.setUsers(
+            [
+                {
+                    id: 1,
+                    fullName: 'Vasya',
+                    followStatus: false,
+                    profileStatus: 'lorem1',
+                    location: {country: 'Ukraine', city: 'Kyiv'}
+                },
+                {
+                    id: 2,
+                    fullName: 'Petro',
+                    followStatus: false,
+                    profileStatus: 'lorem2',
+                    location: {country: 'Belarus', city: 'Minsk'}
+                },
+                {
+                    id: 3,
+                    fullName: 'Svetlana',
+                    followStatus: true,
+                    profileStatus: 'lorem3',
+                    location: {country: 'Hungary', city: 'Budapest'}
+                },
+                {
+                    id: 4,
+                    fullName: 'Dmytro',
+                    followStatus: false,
+                    profileStatus: 'lorem4',
+                    location: {country: 'Japan', city: 'Tokyo'}
+                },
+                {
+                    id: 5,
+                    fullName: 'Pavel',
+                    followStatus: true,
+                    profileStatus: 'lorem5',
+                    location: {country: 'China', city: 'Shanghai'}
+                },
+                {
+                    id: 6,
+                    fullName: 'Anna',
+                    followStatus: true,
+                    profileStatus: 'lorem6',
+                    location: {country: 'USA', city: 'Chicago'}
+                },
+            ]
+        )
+    }
+
     return (
         <div className={styles.usersAreaContainer}>
             {
--- src/store/users-page-reducer.js
+++ src/store/users-page-reducer.js
@@ -2,50 +2,7 @@ const TOGGLE_FOLLOW_ACTION = 'TOGGLE_FOLLOW_ACTION';
 const SET_USERS = 'SET_USERS';
 
 let initialState = {
-    usersData: [
-        {
-            id: 1,
-            fullName: 'Vasya',
-            followStatus: false,
-            profileStatus: 'lorem1',
-            location: {country: 'Ukraine', city: 'Kyiv'}
-        },
-        {
-            id: 2,
-            fullName: 'Petro',
-            followStatus: false,
-            profileStatus: 'lorem2',
-            location: {country: 'Belarus', city: 'Minsk'}
-        },
-        {
-            id: 3,
-            fullName: 'Svetlana',
-            followStatus: true,
-            profileStatus: 'lorem3',
-            location: {country: 'Hungary', city: 'Budapest'}
-        },
-        {
-            id: 4,
-            fullName: 'Dmytro',
-            followStatus: false,
-            profileStatus: 'lorem4',
-            location: {country: 'Japan', city: 'Tokyo'}
-        },
-        {
-            id: 5,
-            fullName: 'Pavel',
-            followStatus: true,
-            profileStatus: 'lorem5',
-            location: {country: 'China', city: 'Shanghai'}
-        },
-        {
-            id: 6,
-            fullName: 'Anna',
-            followStatus: true,
-            profileStatus: 'lorem6',
-            location: {country: 'USA', city: 'Chicago'}
-        },
-    ],
+    usersData: [],
 };
 
 const usersPageReducer = (state = initialState, action) => {
