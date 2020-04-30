f9b999b1030243d30c4435e05f37ae8142a013d6
Yana <zzizzu@ukr.net>
2020-04-30 12:13:55 +0300

    created axios get request, fixed user props, fixed user image styles

--- src/components/MainPage/UsersPage/UsersArea/UsersArea.jsx
+++ src/components/MainPage/UsersPage/UsersArea/UsersArea.jsx
@@ -1,55 +1,16 @@
 import React from "react";
 import styles from './UsersArea.module.scss';
+import * as axios from "axios";
+import userDefaultImage from "../../../../assets/images/pug.jpg";
 
 const UsersArea = (props) => {
 
     if (props.usersData.length === 0) {
-        props.setUsers(
-            [
-                {
-                    id: 1,
-                    fullName: 'Vasya',
-                    followStatus: false,
-                    profileStatus: 'lorem1',
-                    location: {country: 'Ukraine', city: 'Kyiv'}
-                },
-                {
-                    id: 2,
-                    fullName: 'Petro',
-                    followStatus: false,
-                    profileStatus: 'lorem2',
-                    location: {country: 'Belarus', city: 'Minsk'}
-                },
-                {
-                    id: 3,
-                    fullName: 'Svetlana',
-                    followStatus: true,
-                    profileStatus: 'lorem3',
-                    location: {country: 'Hungary', city: 'Budapest'}
-                },
-                {
-                    id: 4,
-                    fullName: 'Dmytro',
-                    followStatus: false,
-                    profileStatus: 'lorem4',
-                    location: {country: 'Japan', city: 'Tokyo'}
-                },
-                {
-                    id: 5,
-                    fullName: 'Pavel',
-                    followStatus: true,
-                    profileStatus: 'lorem5',
-                    location: {country: 'China', city: 'Shanghai'}
-                },
-                {
-                    id: 6,
-                    fullName: 'Anna',
-                    followStatus: true,
-                    profileStatus: 'lorem6',
-                    location: {country: 'USA', city: 'Chicago'}
-                },
-            ]
-        )
+        axios
+            .get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items)
+            })
     }
 
     return (
@@ -57,21 +18,28 @@ const UsersArea = (props) => {
             {
                 props.usersData.map(user =>
                     <div className={styles.userInstance} key={user.id}>
-                        <div className={styles.userInstanceAvatar}></div>
+                        <div className={styles.userInstanceAvatar}>
+                            <img src={user.photos.small != null ? user.photos.small : userDefaultImage}
+                                 alt={user.name}/>
+                        </div>
                         <div className={styles.userInstanceInfo}>
                             <ul>
                                 <li className={styles.userInstanceInfoName}>
-                                    <a href="">{user.fullName}</a>
+                                    <a href="">{user.name}</a>
                                 </li>
-                                <li>{user.location.city}, {user.location.country}</li>
-                                <li className={styles.userInstanceInfoStatus}>{user.profileStatus}</li>
+                                <li>{"user.location.city"}, {"user.location.country"}</li>
+                                <li className={styles.userInstanceInfoStatus}>{user.status}</li>
                             </ul>
                         </div>
                         <div className={styles.userInstanceButton}>
                             {
                                 user.followStatus
-                                    ? <button className={styles.unfollowButton} onClick={() => {props.toogleFollowAction(user.id)}}>Unfollow</button>
-                                    : <button className={styles.followButton} onClick={() => {props.toogleFollowAction(user.id)}}>Follow</button>
+                                    ? <button className={styles.unfollowButton} onClick={() => {
+                                        props.toogleFollowAction(user.id)
+                                    }}>Unfollow</button>
+                                    : <button className={styles.followButton} onClick={() => {
+                                        props.toogleFollowAction(user.id)
+                                    }}>Follow</button>
                             }
                         </div>
                     </div>)
