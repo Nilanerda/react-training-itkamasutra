ccb518faaa45e0a4e31b4b1e6976b44efe6a4946
Yana <zzizzu@ukr.net>
2020-05-03 11:05:15 +0300

    rewritten component into class

--- src/components/MainPage/UsersPage/UsersArea/UsersArea.jsx
+++ src/components/MainPage/UsersPage/UsersArea/UsersArea.jsx
@@ -3,52 +3,54 @@ import styles from './UsersArea.module.scss';
 import * as axios from "axios";
 import userDefaultImage from "../../../../assets/images/pug.jpg";
 
-const UsersArea = (props) => {
+class UsersArea extends React.Component {
 
-    let initializeUsers = () => {
-        if (props.usersData.length === 0) {
+    initializeUsers = () => {
+        if (this.props.usersData.length === 0) {
             axios
                 .get("https://social-network.samuraijs.com/api/1.0/users")
                 .then(response => {
-                    props.setUsers(response.data.items)
+                    this.props.setUsers(response.data.items)
                 })
         }
     }
 
-    return (
-        <div className={styles.usersAreaContainer}>
-            <button onClick={initializeUsers}>Get users</button>
-            {
-                props.usersData.map(user =>
-                    <div className={styles.userInstance} key={user.id}>
-                        <div className={styles.userInstanceAvatar}>
-                            <img src={user.photos.small != null ? user.photos.small : userDefaultImage}
-                                 alt={user.name}/>
-                        </div>
-                        <div className={styles.userInstanceInfo}>
-                            <ul>
-                                <li className={styles.userInstanceInfoName}>
-                                    <a href="">{user.name}</a>
-                                </li>
-                                <li>{"user.location.city"}, {"user.location.country"}</li>
-                                <li className={styles.userInstanceInfoStatus}>{user.status}</li>
-                            </ul>
-                        </div>
-                        <div className={styles.userInstanceButton}>
-                            {
-                                user.followStatus
-                                    ? <button className={styles.unfollowButton} onClick={() => {
-                                        props.toogleFollowAction(user.id)
-                                    }}>Unfollow</button>
-                                    : <button className={styles.followButton} onClick={() => {
-                                        props.toogleFollowAction(user.id)
-                                    }}>Follow</button>
-                            }
-                        </div>
-                    </div>)
-            }
-        </div>
-    )
+    render() {
+        return (
+            <div className={styles.usersAreaContainer}>
+                <button onClick={this.initializeUsers}>Get users</button>
+                {
+                    this.props.usersData.map(user =>
+                        <div className={styles.userInstance} key={user.id}>
+                            <div className={styles.userInstanceAvatar}>
+                                <img src={user.photos.small != null ? user.photos.small : userDefaultImage}
+                                     alt={user.name}/>
+                            </div>
+                            <div className={styles.userInstanceInfo}>
+                                <ul>
+                                    <li className={styles.userInstanceInfoName}>
+                                        <a href="">{user.name}</a>
+                                    </li>
+                                    <li>{"user.location.city"}, {"user.location.country"}</li>
+                                    <li className={styles.userInstanceInfoStatus}>{user.status}</li>
+                                </ul>
+                            </div>
+                            <div className={styles.userInstanceButton}>
+                                {
+                                    user.followStatus
+                                        ? <button className={styles.unfollowButton} onClick={() => {
+                                            this.props.toogleFollowAction(user.id)
+                                        }}>Unfollow</button>
+                                        : <button className={styles.followButton} onClick={() => {
+                                            this.props.toogleFollowAction(user.id)
+                                        }}>Follow</button>
+                                }
+                            </div>
+                        </div>)
+                }
+            </div>
+        )
+    }
 }
 
 export default UsersArea;
