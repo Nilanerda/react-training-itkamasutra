46cc26722da7b580400887a7e071ff22c1d55b1d
Yana <zzizzu@ukr.net>
2020-05-05 11:39:18 +0300

    created new action creators for pagination, improved initial state, expanded reducer

--- src/store/users-page-reducer.js
+++ src/store/users-page-reducer.js
@@ -1,14 +1,21 @@
 const TOGGLE_FOLLOW_ACTION = 'TOGGLE_FOLLOW_ACTION';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
     usersData: [],
+    shownUsers: 10,
+    totalIncomeUsersCount: 0,
+    currentPage: 1,
 };
 
 const usersPageReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_USERS: {
-            return {...state, usersData: [...state.usersData, ...action.users]}
+            return {
+                ...state, usersData: action.users
+            }
         }
         case TOGGLE_FOLLOW_ACTION:
             return {
@@ -20,6 +27,16 @@ const usersPageReducer = (state = initialState, action) => {
                     return users;
                 })
             }
+        case SET_CURRENT_PAGE:
+            return {
+                ...state,
+                currentPage: action.currentPage
+            }
+        case SET_TOTAL_USERS_COUNT:
+            return {
+                ...state,
+                totalIncomeUsersCount: action.totalUsersCount
+            }
         default:
             return state;
     }
@@ -28,5 +45,9 @@ const usersPageReducer = (state = initialState, action) => {
 export const toggleFollowActionCreator = (userId) => ({type: TOGGLE_FOLLOW_ACTION, userId});
 
 export const setUsersActionCreator = (users) => ({type: SET_USERS, users});
+
+export const setCurrentPageCreator = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
+
+export const setTotalUsersCountCreator = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, totalUsersCount})
 
 export default usersPageReducer;
