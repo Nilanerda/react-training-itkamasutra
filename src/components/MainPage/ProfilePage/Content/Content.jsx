dcfda66c5b5a9f6583c10746dad7a16761161f11
Yana <zzizzu@ukr.net>
2020-05-11 15:27:44 +0300

    fixed media styles

--- src/components/MainPage/ProfilePage/Content/Content.jsx
+++ src/components/MainPage/ProfilePage/Content/Content.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import './Content.scss';
 import UserInfo from "./UserInfo/UserInfo";
 import PostsContainer from "./Posts/PostsContainer";
+import Wallpaper from "./Wallpaper/Wallpaper";
 
 const Content = () => {
     return (
         <div className="main-info-wrapper">
+            <Wallpaper/>
             <UserInfo/>
             <PostsContainer />
         </div>
--- src/components/MainPage/ProfilePage/ProfilePage.jsx
+++ src/components/MainPage/ProfilePage/ProfilePage.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import styles from "./ProfilePage.module.scss";
-import Wallpaper from "./Wallpaper/Wallpaper";
 import Content from "./Content/Content";
 
 const ProfilePage = () => {
     return (
         <div className={styles.innerContainer}>
-            <Wallpaper/>
             <Content />
         </div>
     )
