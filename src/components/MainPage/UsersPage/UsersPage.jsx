7adc4c01de4c238444e73ec132a52da29b4da913
Yana <zzizzu@ukr.net>
2020-05-11 15:45:41 +0300

    deleted unused styles

--- src/components/MainPage/UsersPage/UsersPage.jsx
+++ src/components/MainPage/UsersPage/UsersPage.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import styles from './UsersPage.module.scss';
 import UsersAreaContainer from "./UsersArea/UsersAreaContainer";
 import UsersPagePaginationContainer from "./UsersPagePagination/UsersPagePaginationContainer";
 
 let UsersPage = () => {
     return (
-        <div className={styles.innerContainer}>
+        <div>
             <UsersAreaContainer/>
             <UsersPagePaginationContainer/>
         </div>
