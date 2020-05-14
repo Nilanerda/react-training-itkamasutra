b21a8a4e72f651ed2753dd6d612282bbd984d6c0
Yana <zzizzu@ukr.net>
2020-05-14 09:43:47 +0300

    created new case to reducer for set users

--- src/store/profile-page-reducer.js
+++ src/store/profile-page-reducer.js
@@ -1,5 +1,6 @@
 const ADD_POST = 'ADD_POST';
 const UPDATE_NEW_POST_DATA = 'UPDATE_NEW_POST_DATA';
+const  SET_USER_PROFILE = 'SET_USER_PROFILE';
 
 let initialState = {
     postsData: [
@@ -16,6 +17,7 @@ let initialState = {
             post: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda atque dicta dolore dolorum ex id in necessitatibus numquam pariatur perspiciatis praesentium quae quam rem soluta tempora temporibus vel vero, voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad adipisci aliquam assumenda consectetur dolorum ipsam omnis, ratione saepe veniam! Assumenda corporis inventore iusto labore magni quam quisquam repellat sint! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet at explicabo harum iste nemo quam quisquam sequi sunt vero! Blanditiis dolorem, est exercitationem magnam minus molestiae nemo temporibus voluptatem?'
         }
     ],
+    profile: null,
     newPostData: ''
 };
 
@@ -33,6 +35,11 @@ const profilePageReducer = (state = initialState, action) => {
                 ...state,
                 newPostData: action.updatedValue
             }
+        case SET_USER_PROFILE:
+            return {
+                ...state,
+                profile: action.profile
+            }
         default:
             return state;
     }
@@ -44,5 +51,7 @@ export const updateNewPostData = (currentPostValue) => ({
     type: UPDATE_NEW_POST_DATA,
     updatedValue: currentPostValue
 });
+
+export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
 
 export default profilePageReducer;
